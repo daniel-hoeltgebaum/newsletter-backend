6b064d0eda3499699bb251c8ffd8478d06202b99
Daniel Höltgebaum <daniel.hoeltgebaum@gmx.net>
2025-02-13 14:01:41 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,33 @@ import { Hono } from "hono";
 import { subscriber } from "./routes/subscriber";
 import { newsletter } from "./routes/newsletter";
 
+import * as os from "os";
+import { serve } from "@hono/node-server";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const app = new Hono();
+const PORT = 8080;
 
 // connect routes
 app.route("/subscriber", subscriber);
 app.route("/newsletter", newsletter);
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text(`Hello from ${os.hostname()}!`);
 });
 
-export default app;
+// to run with node
+if (!process.versions.bun) {
+  // check if not running on bun
+  console.log(`Server running on ${PORT}`);
+  serve({
+    fetch: app.fetch,
+    port: PORT,
+  });
+}
+
+export default {
+  fetch: app.fetch,
+  port: PORT,
+};
